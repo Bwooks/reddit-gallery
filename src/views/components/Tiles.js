153d0dc230a840270132744ec378e528aa497a4b
Brooks Dulla <brooksdulla@gmail.com>
2017-06-18 15:20:05 -0400

    Testing react-grid-gallery

--- src/views/components/Tiles.js
+++ src/views/components/Tiles.js
@@ -2,15 +2,14 @@ import Tile from './Tile.js';
 import React from 'react';
 
 const Tiles = ({images}) => {
-    let tileNum = 0;
     return (
         <div>
-            {images.map((image)=>{
+            {images.map((image, currentIndex)=>{
                 const containsImage = /imgur/g.test(image.data.domain);
                 if(containsImage) {
-                    tileNum++;
-                    return <Tile image={image.data} tileNum = {tileNum} key = {tileNum}/>
-                }
+                    console.log(image);
+                    return <Tile image={image.data} key = {currentIndex}/>
+                }                
             })}
         </div>
     );
